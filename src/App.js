5d99aa4871b8ef5de2fa899d5d016c4bed3a81b8
balaji <krishbalaji1998@gmail.com>
2022-07-01 00:15:17 +0530

    table styling, refactor

--- src/App.js
+++ src/App.js
@@ -9,7 +9,34 @@ import {useState} from 'react';
 
 function App() {
 
-  const [count,set]=useState({})
+
+  const[count,set]=useState(null)
+  const [a,setA]=useState([
+    {
+        header:'react', 
+        catagory: 'web',
+        deadline: '10/07/2022'
+    },
+    {
+        header:'tree', 
+        catagory: 'code',
+        deadline: '30/07/2022'
+    },
+    {
+        header:'list & array', 
+        catagory: 'code',
+        deadline: '30/07/2022'
+    },
+    {
+        header:'update mobile no in pan', 
+        catagory: 'personal',
+        deadline: '12/07/2022'
+    },
+    {
+        header:'javascript', 
+        catagory: 'web',
+        deadline: '30/07/2022'
+    }])
   const parent=(e)=>{
     set(e)
     console.log(e)
@@ -18,7 +45,7 @@ function App() {
     <div>
     <Todo />
     <Switch>
-    <Route exact path="/"><Home parent={parent}></Home><Detail item={count}></Detail></Route>
+    <Route exact path="/"><Home parent={parent} a={a}></Home><Detail item={count}></Detail></Route>
       <Route path="/detail"><Detail item={count}></Detail></Route>
     </Switch>
     </div>
